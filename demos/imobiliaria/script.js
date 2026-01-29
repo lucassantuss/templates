a2d3d2f1abe294520f7a471d59cc9e2616832d28
Lucas Araujo dos Santos <lucassantuss@outlook.com>
2026-01-29 16:53:33 -0300

    ajustes visuais paginas templates

--- demos/imobiliaria/script.js
+++ demos/imobiliaria/script.js
@@ -3,20 +3,22 @@ const toggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 
 toggle.addEventListener('click', () => {
-    menu.classList.toggle('open');
+  menu.classList.toggle('open');
 });
 
-// Reveal on Scroll
+// Reveal on scroll
 const reveals = document.querySelectorAll('.reveal');
 
 function revealOnScroll() {
-    reveals.forEach(el => {
-        const windowHeight = window.innerHeight;
-        const elementTop = el.getBoundingClientRect().top;
-        if (elementTop < windowHeight - 100) {
-            el.classList.add('active');
-        }
-    });
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const visible = 100;
+
+    if (elementTop < windowHeight - visible) {
+      el.classList.add('active');
+    }
+  });
 }
 
 window.addEventListener('scroll', revealOnScroll);
